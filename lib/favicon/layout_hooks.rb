ef46eeb70eed2fd59d76a01c1bb9e4f2dcd62b55
Krishna Srihari <krishna.srihari@gmail.com>
2012-07-18 20:13:54 +0530

    fix dispatcher and faviconHelper

--- lib/favicon/layout_hooks.rb
+++ lib/favicon/layout_hooks.rb
@@ -1,3 +1,5 @@
+require File.expand_path(File.dirname(__FILE__) + '/../../app/helpers/favicon_helper')
+
 module Favicon
   class LayoutHooks < Redmine::Hook::ViewListener
     include FaviconHelper
